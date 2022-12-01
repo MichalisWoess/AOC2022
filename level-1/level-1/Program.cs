15d9c898defe62356f1e61f372e9964e8ba91ff2
Michalis Wöss <michaliswoess@gmail.com>
2022-12-01 10:37:23 +0100

    create solution to level 1 (a and b)

---
+++ level-1/level-1/Program.cs
@@ -0,0 +1,19 @@
+﻿
+var file = File.ReadAllLines("input.txt");
+
+List<int> calorieSums = new();
+int calorieSum = 0;
+foreach (var calorieCount in file)
+{
+    if(calorieCount != "")
+        calorieSum += int.Parse(calorieCount);
+    else
+    {
+        calorieSums.Add(calorieSum);
+        calorieSum = 0;
+    }
+}
+
+Console.WriteLine(calorieSums.Max());
+// sum of top three
+Console.WriteLine(calorieSums.OrderByDescending(x => x).Take(3).Sum());
