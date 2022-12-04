2ee2f87d72a1964613cd576aafe6cf88d50cc360
Michalis Wöss <michaliswoess@gmail.com>
2022-12-04 15:32:02 +0100

    Level 4

---
+++ level-4/level-4/Program.cs
@@ -0,0 +1,17 @@
+﻿var file = File.ReadAllLines("input");
+
+var overlapCounter = 0;
+foreach (var line in file)
+{
+    var splitArr = line.Split(',');
+    var pair1 = splitArr[0].Split('-').Select(int.Parse).ToArray();
+    var pair2 = splitArr[1].Split('-').Select(int.Parse).ToArray();
+
+    if ((pair1[1] <= pair2[1] && pair1[1] >= pair2[0]) ||
+        (pair1[0] >= pair2[0] && pair2[1] >= pair1[0]) ||
+        (pair1[0] <= pair2[0] && pair1[1] >= pair2[1]) ||
+        (pair2[0] <= pair1[0] && pair2[1] >= pair1[1]))
+        overlapCounter++;
+}
+
+Console.WriteLine(overlapCounter);
