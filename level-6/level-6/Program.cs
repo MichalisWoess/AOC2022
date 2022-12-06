6fa964cf938e7753c40a64d100c50377593a062f
Michalis Wöss <michaliswoess@gmail.com>
2022-12-06 12:25:05 +0100

    Level 6 A

---
+++ level-6/level-6/Program.cs
@@ -0,0 +1,11 @@
+﻿var line = File.ReadAllText("input");
+
+for (int i = 0; i < line.Length - 4; i++)
+{
+    var slice = line.Substring(i, 4);
+    if (slice.Distinct().Count() == 4)
+    {
+        Console.WriteLine(i +4);
+        break;
+    } 
+}
